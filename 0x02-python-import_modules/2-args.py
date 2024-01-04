600cfd03fd31b94ab86eb693b45774d83848c4c1
Lamba237 <simontanyi2004@gmail.com>
2024-01-04 15:30:11 -0800

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-def print_Argv(argv):
+def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
-        return
+        #return
     else:
         if n == 1:
             print("{:d} argument:".format(n))
         else:
+            print("{:d} arguments:".format(n))
             i = 1
             while i <= n:
                 print("{:d}: {:s}".format(i, argv[i]))
@@ -15,4 +16,4 @@ def print_Argv(argv):
 
                 if __name__ == "__main__":
                     import sys
-                    print_Argv(sys.argv)
+                    print_arg(sys.argv)
