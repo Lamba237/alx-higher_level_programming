786a53e52a3cc3ec270f05a79e3083d8d4d426f5
Lamba237 <simontanyi2004@gmail.com>
2024-01-24 15:30:03 -0800

    updates

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class for square"""
+
+
+class Square:
+    """
+       This is an empty class `Square` that defines a square
+       """
+    def __init__(self, size):
+        """
+        param size: size of the square
+        """
+        self.__size = size
