e16b00075155b51c0735b37f5430180db7049f88
Lamba237 <simontanyi2004@gmail.com>
2024-01-24 15:53:26 -0800

    updates

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class for square"""
+
+
+class Square:
+    """
+       This is an empty class `Square` that defines a square
+       """
+    def __init__(self, size=0):
+        """
+        param size: size of the square
+        """
+        self.__size = size
+
+        if isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
