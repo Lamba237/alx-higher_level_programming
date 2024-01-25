9611325699dbf96e649b9d9325ffa2fc7bc315b2
Lamba237 <simontanyi2004@gmail.com>
2024-01-24 16:08:59 -0800

    updates

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,9 +10,9 @@ class Square:
         """
         param size: size of the square
         """
-        self.__size = size
-
+   
         if isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
