f320ed5557451ad95e68db1cffab5033d98989f4
Lamba237 <simontanyi2004@gmail.com>
2024-01-29 14:14:00 -0800

    updates

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
 
 
 class Rectangle:
-    """
-    A class that is used to calculate for rectangle
-    """
+    """ class rectangle"""
     pass
