ba106f3ce96783f1246edf4775453a3cf84e5ecd
Lamba237 <simontanyi2004@gmail.com>
2024-01-31 17:04:25 -0800

    updates

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Class LockedClass
+"""
+
+
 class LockedClass:
+    """
+    This class raises an error when an instance attribute apart
+    from first_name is used
+    """
     __slots__ = ['first_name']
