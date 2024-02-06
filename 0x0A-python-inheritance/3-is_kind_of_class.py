3dafa6a519fb391fcd8fcdc0e1f402900b5206da
Lamba237 <simontanyi2004@gmail.com>
2024-02-05 17:32:22 -0800

    updates

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,4 @@ def is_kind_of_class(obj, a_class):
     :param a_class: a class
     :return:
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
