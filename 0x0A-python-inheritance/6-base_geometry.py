74f1a90a6a5d00c936ac9d06e6887c8c5181aae6
Lamba237 <simontanyi2004@gmail.com>
2024-02-05 18:29:41 -0800

    updates

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Class called BaseGeometry
+    """
+    def area(self):
+        """
+        :return: nothing
+        """
+        raise Exception("area() is not implemented")
