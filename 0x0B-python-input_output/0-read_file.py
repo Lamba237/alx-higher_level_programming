e2a0cc4059b5976d410fff6580fc2953ed43e005
Lamba237 <simontanyi2004@gmail.com>
2024-02-07 15:17:24 -0800

    updates task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function to open a file for reading
+"""
+
+
+def read_file(filename=""):
+    """
+    :param filename: name of file to be read from
+    :return: nothing
+    """
+    with open(filename, encoding="utf-8") as f:
+        f_r = f.read()
+        print(f_r)
