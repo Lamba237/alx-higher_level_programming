e414d5215be4404b891a8b68a7ef13e30ce34581
Lamba237 <simontanyi2004@gmail.com>
2024-02-07 15:56:54 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""
+a function that returns the JSON
+representation of an object (string):
+"""
+
+
+def to_json_string(my_obj):
+    """
+    :param my_obj: dictionary
+    :return: JSON representation of object
+    """
+
+    return json.dumps(my_obj)
