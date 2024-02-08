35518a6aa23f99555f728079c5ae3626b2d045de
Lamba237 <simontanyi2004@gmail.com>
2024-02-07 16:19:55 -0800

    updates

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import json
+
+"""
+function that returns an
+object (Python data structure)
+represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+
+    :param my_str: string to be return
+    :return: a string
+    """
+    return json.dumps(my_str)
