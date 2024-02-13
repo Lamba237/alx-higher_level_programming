bdd0a07e57a2f81483d0eea289482b0f572b52aa
Lamba237 <simontanyi2004@gmail.com>
2024-02-12 18:26:13 -0800

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ class called base
 """
 
 
+import json
+
+
 class Base:
     """
     base class, that defines a private class attribute call
@@ -21,3 +24,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns JSON representation of list"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
