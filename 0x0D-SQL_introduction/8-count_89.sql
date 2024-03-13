39c1b09ba3de0ab7e6fca0b069a37adcaf7d5dd7
Lamba237 <simontanyi2004@gmail.com>
2024-03-13 06:40:40 -0700

    Made an update

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,5 +2,6 @@
 -- id = 89 in the table first_table of the database 
 -- hbtn_0c_0 in your MySQL server.
 
-SELECT COUNT WHERE (id = 89)
-FROM first_table;
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
