70d8df6d3eb99d6656f65b8fbaa0df68e1880bc1
Lamba237 <simontanyi2004@gmail.com>
2024-03-13 06:17:16 -0700

    Made an update

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- first_table from the database hbtn_0c_0 in your MySQL server.
 -- (.) The database name will be passed as an argument of the mysql command
 
-SHOW TABLE first_table;
+SELECT * FROM TABLE first_table;
