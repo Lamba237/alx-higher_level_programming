8c897267638ffc291eb6bc077423f8eed2bda0bb
Lamba237 <simontanyi2004@gmail.com>
2024-03-13 07:14:08 -0700

    Made an update

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- A script that List all records of the table
 
-SELECT `score`, `name` FROM `second_table` ORDER BY 'score' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY 'score' DESC;
