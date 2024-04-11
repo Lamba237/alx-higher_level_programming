3542c357cc8f5185df512f744e7d1232fe17303f
Lamba237 <simontanyi2004@gmail.com>
2024-04-11 05:34:14 -0700

    updates for task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Class called Rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+	  if (w > 0 && h > 0) {
+		  this.weight = w;
+		  this.height = h;
+	  }
+  }
+}
