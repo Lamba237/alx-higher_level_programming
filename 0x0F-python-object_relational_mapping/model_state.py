9b5cbe71e59147c813f8b494cf7d56d8c276a30b
Lamba237 <simontanyi2004@gmail.com>
2024-04-14 18:31:17 -0700

    updates

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""List"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+engine = create_engine('mysql://username:password@localhost:3306/database')
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
