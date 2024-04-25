b56db73134a4044911e0d6786f129b626296279a
Lamba237 <simontanyi2004@gmail.com>
2024-04-25 16:45:29 -0700

    updates

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #send a GET request to the URL
 
-response=$(curl -s -o /dev/null -w "%{http_code}" $1)
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
 
 if [ "$response" -eq 200 ]; then
-	body=$(curl -s $1)
+	body=$(curl -s "$1")
 	echo "$body"
 fi
