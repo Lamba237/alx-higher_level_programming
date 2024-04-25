2dab61c1e9d9ede39f3fb3f780d5a220b887c7af
Lamba237 <simontanyi2004@gmail.com>
2024-04-25 16:51:17 -0700

    updates

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 #send a GET request to the URL
-
-response=$(curl -L -s -X HEAD -w "%{http_code}" "$1")
-
-if [ "$response" == 200 ]; then
-	curl -Ls "$1";
-fi
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
