1385d0f250cecacf652243b8257802e9a558e07e
Lamba237 <simontanyi2004@gmail.com>
2024-04-26 12:05:43 -0700

    updates

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-cul -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "origin:school"
+curl -s -L -X PUT -d "user_id=98" -H "origin:school" 0.0.0.0:5000/catch_me
