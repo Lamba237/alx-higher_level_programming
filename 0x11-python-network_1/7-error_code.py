21763c43f7f95b230900783f3d40c46fedee5f8e
Lamba237 <simontanyi2004@gmail.com>
2024-04-28 19:43:33 -0700

    updates

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+HTTP status code using requests module
+"""
+
+import requests
+import sys
+
+
+def HTTP_status_code():
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+
+
+if __name__ == '__main__':
+    HTTP_status_code()
