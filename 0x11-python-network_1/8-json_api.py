62e2025ace0ff20e85e3910c921feeb592f99a29
Lamba237 <simontanyi2004@gmail.com>
2024-04-28 20:12:07 -0700

    updates

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,6 @@ def searchUser(letter):
         print("Not a valid JSON")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    searchUser(letter)
