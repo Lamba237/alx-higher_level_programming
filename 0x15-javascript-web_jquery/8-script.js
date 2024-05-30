617508ec8d19ae67d363e9dff031678914bc063e
Lamba237 <simontanyi2004@gmail.com>
2024-05-29 19:16:06 -0700

    updates

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+// script that fetches and lists the title for all
+// movies by using this URL:
+// https://swapi-api.alx-tools.com/api/films/?format=json
+
+$(document).ready(function () {
+  $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $.each(data.results, function (i, movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
+});
