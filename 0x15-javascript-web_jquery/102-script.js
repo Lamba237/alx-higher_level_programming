2d23b728973a01e7685dcf5a3f2549d53d8dad43
Lamba237 <simontanyi2004@gmail.com>
2024-05-29 19:37:04 -0700

    final updates

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+// prints how to say “Hello” depending on the language
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    const lang = $('#language_code').val();
+    const url = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + lang;
+    $.getJSON(url, function (data) {
+      $('#hello').text(data.hello);
+    });
+  });
+});
---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+// prints how to say “Hello” depending on the language
+$(document).ready(function () {
+  function translate () {
+    const lang = $('#language_code').val();
+    const url = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + lang;
+    $.getJSON(url, function (data) {
+      $('#hello').text(data.hello);
+    });
+  }
+
+  $('#btn_translate').click(translate);
+
+  $('#language_code').keypress(function (e) {
+    if (e.which === 13) {
+      translate();
+    }
+  });
+});
