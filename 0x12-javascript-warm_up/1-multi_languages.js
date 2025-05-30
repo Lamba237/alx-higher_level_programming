844f8b6cb4ad9d85af6692593cca5164610dba3d
Lamba237 <simontanyi2004@gmail.com>
2025-05-30 11:51:55 +0100

    new directory created

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-// script that prints 3 lines
-const myVar = 'C is fun';
-console.log(myVar);
-const myVar2 = 'Python is cool';
-console.log(myVar2);
-const myVar3 = 'JavaScript is amazing';
-console.log(myVar3);
